4a012fcb61942b8627cc02f472f5221ea88268dd
Nikhil Prajapati <nikhilprajapati3003@gmail.com>
2025-03-18 18:10:49 +0530

    add trpc

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,21 @@
-import Image from "next/image";
+// "use client";
 
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-export default function Home() {
-  return(
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "world" });
+  return (
     <div>
-      Videos come here
+      <HydrateClient>
+        <Suspense fallback={<p>loading...</p>}>
+          <ErrorBoundary fallback={<p>Error...</p>}>
+            <PageClient />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrateClient>
     </div>
-  )
+  );
 }
